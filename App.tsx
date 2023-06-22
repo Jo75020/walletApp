dae5436410e1d5954c8540a3b674bbf6b2c3f5f7
jo75020 <jordand75020@hotmail.fr>
2023-06-22 15:47:50 +0200

    CleanView to handle RootNavigator

--- App.tsx
+++ App.tsx
@@ -1,26 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { app } from './firebaseConfig';
-import Web3 from 'web3';
-import { useEffect } from 'react';
-
-const web3 = new Web3(
-  new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/b19e871032b943bb988d4920d62c875a')
-);
-
-web3.eth.getBlockNumber().then( blockNumber => {
-  console.log('lastBlock', blockNumber);
-});
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
 
-  useEffect(() => {
-    console.log('firebase', app);
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
